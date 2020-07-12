fafcd7f73fe813b32cafd67d67f1001d9dfc50b9
Diwaker Singh <diwaker020100singh@gmail.com>
2020-07-12 15:05:11 +0530

    First Maze addition

---
+++ site/script.js
@@ -0,0 +1,19 @@
+
+
+var Rows = 25;
+var Cols = 40;
+
+function generateGrid( rows, cols ) {
+    var grid = "<table>";
+    for ( row = 1; row <= rows; row++ ) {
+        grid += "<tr>"; 
+        for ( col = 1; col <= cols; col++ ) {      
+            grid += "<td></td>";
+        }
+        grid += "</tr>"; 
+    }
+    grid += "</table>"
+    return grid;
+}
+
+$("#maze").append(generateGrid(Rows,Cols));
