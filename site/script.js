d1a0db95fd0c52809da7c89d2a25413390a0919a
Diwaker Singh <diwaker020100singh@gmail.com>
2020-07-12 23:39:08 +0530

    Wall Creation Javascript added

--- site/script.js
+++ site/script.js
@@ -2,6 +2,17 @@
 
 var Rows = 25;
 var Cols = 40;
+var inProgress = false;
+var cellsToAnimate = [];
+var createWalls = false;
+var algorithm = null;
+var justFinished = false;
+var animationSpeed = "Fast";
+var animationState = null;
+var startCell = [11, 15];
+var endCell = [11, 25];
+var movingStart = false;
+var movingEnd = false;
 
 function generateGrid( rows, cols ) {
     var grid = "<table>";
@@ -17,3 +28,72 @@ function generateGrid( rows, cols ) {
 }
 
 $("#maze").append(generateGrid(Rows,Cols));
+
+
+$( "td" ).mousedown(function(){
+	var index = $( "td" ).index( this );
+	var startCellIndex = (startCell[0] * (Cols)) + startCell[1];
+	var endCellIndex = (endCell[0] * (Cols)) + endCell[1];
+	if ( !inProgress ){
+		// Clear board if just finished
+		if ( justFinished  && !inProgress ){ 
+			clearBoard( keepWalls = true ); 
+			justFinished = false;
+		}
+		if (index == startCellIndex){
+			movingStart = true;
+			//console.log("Now moving start!");
+		} else if (index == endCellIndex){
+			movingEnd = true;
+			//console.log("Now moving end!");
+		} else {
+			createWalls = true;
+		}
+	}
+});
+
+$( "td" ).mouseup(function(){
+	createWalls = false;
+	movingStart = false;
+	movingEnd = false;
+});
+
+$( "td" ).mouseenter(function() {
+	if (!createWalls && !movingStart && !movingEnd){ return; }
+    var index = $( "td" ).index( this );
+    var startCellIndex = (startCell[0] * (Cols)) + startCell[1];
+	var endCellIndex = (endCell[0] * (Cols)) + endCell[1];
+    if (!inProgress){
+    	if (justFinished){ 
+    		clearBoard( keepWalls = true );
+    		justFinished = false;
+    	}
+    	//console.log("Cell index = " + index);
+    	if (movingStart && index != endCellIndex) {
+    		moveStartOrEnd(startCellIndex, index, "start");
+    	} else if (movingEnd && index != startCellIndex) {
+    		moveStartOrEnd(endCellIndex, index, "end");
+    	} else if (index != startCellIndex && index != endCellIndex) {
+    		$(this).toggleClass("wall");
+    	}
+    }
+});
+
+$( "td" ).click(function() {
+    var index = $( "td" ).index( this );
+    var startCellIndex = (startCell[0] * (Cols)) + startCell[1];
+	var endCellIndex = (endCell[0] * (Cols)) + endCell[1];
+    if ((inProgress == false) && !(index == startCellIndex) && !(index == endCellIndex)){
+    	if ( justFinished ){ 
+    		clearBoard( keepWalls = true );
+    		justFinished = false;
+    	}
+    	$(this).toggleClass("wall");
+    }
+});
+
+$( "body" ).mouseup(function(){
+	createWalls = false;
+	movingStart = false;
+	movingEnd = false;
+});
